74a22021dbb5f042cbb88fc8236636fbedef3743
summertime-fms <danyushin0107@gmail.com>
2020-05-01 16:13:40 +0300

    automotization

--- gulpfile.js
+++ gulpfile.js
@@ -6,7 +6,15 @@ var sourcemap = require("gulp-sourcemaps");
 var less = require("gulp-less");
 var postcss = require("gulp-postcss");
 var autoprefixer = require("autoprefixer");
+var csso = require("gulp-csso");
+var rename = require("gulp-rename");
+var imagemin = require("gulp-imagemin");
+var webp = require("gulp-webp");
+var svgstore = require("gulp-svgstore");
+var posthtml = require("gulp-posthtml");
+
 var server = require("browser-sync").create();
+
 
 gulp.task("css", function () {
   return gulp.src("source/less/style.less")
@@ -16,8 +24,10 @@ gulp.task("css", function () {
     .pipe(postcss([
       autoprefixer()
     ]))
+    .pipe(csso())
+    .pipe(rename("style.min.css"))
     .pipe(sourcemap.write("."))
-    .pipe(gulp.dest("source/css"))
+    .pipe(gulp.dest("build/css"))
     .pipe(server.stream());
 });
 
@@ -34,4 +44,56 @@ gulp.task("server", function () {
   gulp.watch("source/*.html").on("change", server.reload);
 });
 
-gulp.task("start", gulp.series("css", "server"));
+gulp.task("images", function() {
+  return gulp.src("source/img/**/*.{png,jpg,svg}")
+  .pipe(imagemin([
+    imagemin.optipng({optimizationLevel: 1}),
+    imagemin.mozjpeg({progressive: true})
+  ]))
+  .pipe(gulp.dest("source/img"));
+});
+
+gulp.task("webp", function() {
+  return gulp.src("source/img/**/*.{png,jpg}")
+  .pipe(webp({quality: 90}))
+
+  .pipe(gulp.dest("source/img"));
+
+})
+
+gulp.task("sprite", function() {
+  return gulp.src("source/img/icon-*.svg")
+  .pipe(svgstore({
+    inlineSvg: true
+  }))
+  .pipe(rename("sprite.svg"))
+  .pipe(gulp.dest("build/img"));
+})
+
+gulp.task("copy", function() {
+  return gulp.src([
+    "source/fonts/**/*.{woff, woff2}",
+    "source/img/**",
+    "source/js/**",
+    "source/*.ico"
+
+  ], {
+    base: "source"
+  })
+  .pipe(gulp.dest("build"));
+})
+
+gulp.task("clean", function() {
+  return gulp.src("build");
+})
+
+
+gulp.task("html", function() {
+  return gulp.src("source/*.html")
+.pipe(posthtml([
+  require("posthtml-aria-tabs")()
+]))
+  .pipe(gulp.dest("build"));
+});
+gulp.task("build", gulp.series("clean", "copy", "css", "images", "webp", "sprite", "html"))
+gulp.task("start", gulp.series("build", "server"));
