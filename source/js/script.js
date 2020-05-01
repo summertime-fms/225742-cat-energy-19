72e16e7fb51e2ee5239fdc04e752a4a04fc5a36e
summertime-fms <danyushin0107@gmail.com>
2020-05-01 10:55:38 +0300

    pixel perfect

--- source/js/script.js
+++ source/js/script.js
@@ -2,9 +2,9 @@ var button = document.querySelector(".main-nav__toggle");
 var menu = document.querySelector(".main-nav__list");
 
 
-
 button.addEventListener("click", function() {
   menu.classList.toggle("main-nav__list--open");
   button.classList.toggle("main-nav__toggle--close")
 
 });
+
