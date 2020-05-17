9e0a2133ec4c932a6e0ab3c3a701588702eee7df
summertime-fms <danyushin0107@gmail.com>
2020-05-18 01:08:51 +0300

    оживляет слайдер

--- source/js/script.js
+++ source/js/script.js
@@ -10,3 +10,34 @@ button.addEventListener("click", function() {
 
   button.classList.toggle("main-nav__toggle--close")
 });
+
+let btnSlider = document.querySelector(".examples-slider__toggler-range");
+let slider = document.querySelector(".examples-slider__toggler");
+
+btnSlider.ondragstart = function() {
+return false;
+};
+
+btnSlider.onmousedown = function() {
+const sliderClientRect = slider.getBoundingClientRect();
+const borderLeft = sliderClientRect.x;
+const sliderWidth = sliderClientRect.width;
+const halfBtnSlider = btnSlider.offsetWidth / 2 ;
+document.onmousemove = function(event) {
+let x = event.clientX - borderLeft;
+if (x >= 0 && x <= sliderWidth) {
+btnSlider.style.left = x - halfBtnSlider + 'px';
+let slides = document.querySelectorAll(".examples-slider__slide");
+const slideShow = x / sliderWidth * 100;
+slides[0].style.clipPath = 'inset(0 ' + (Math.floor(100 - slideShow)) + '% 0 0';
+slides[1].style.clipPath = 'inset(0 0 0 ' + (Math.ceil(slideShow)) + '%';
+}
+}
+
+function reboot() {
+document.onmousemove = null;
+btnSlider.onmouseup = null;
+}
+btnSlider.onmouseup = reboot;
+document.onmouseup = reboot;
+}
